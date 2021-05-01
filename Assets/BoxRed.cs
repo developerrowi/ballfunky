9c4fb252633c1e147958bef9cd6683d06525dbcf
rowiRedFulcrum <rowell.c@redfulcrum.com>
2021-05-02 00:12:04 +0800

    Update ball movement

---
+++ Assets/BoxRed.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxRed : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public Rigidbody player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("collided");
+        if(GlobalClass.isGoingDown) {
+          GlobalClass.isGoingDown = false;
+          player.AddForce(0, 20100 * Time.deltaTime, 0);
+        }
+
+
+    }
+
+
+}
---
+++ Assets/GlobalClass.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+using System.Collections;
+public class GlobalClass : MonoBehaviour {
+
+       const int ScreenFadingIn = -1;
+       const int ScreenIdle = 0;
+       const int ScreenFadingOut   =   1;
+       public static float ScreenFadeAlpha = 1.0f;
+       public static bool isGoingDown = false;
+}
